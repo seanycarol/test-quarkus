e7cf2977cfbf555386ac35d690ccce18d08c9c88
SeanyCarol <seany@ice.ufjf.br>
2021-11-02 16:27:14 -0300

    Prepare database

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+insert into Fruit(id, name, quantity) values(nextval('hibernate_sequence'), 'Maçã', 5);
+insert into Fruit(id, name, quantity) values(nextval('hibernate_sequence'), 'Uva', 3);
+insert into Fruit(id, name, quantity) values(nextval('hibernate_sequence'), 'Laranja', 4);
